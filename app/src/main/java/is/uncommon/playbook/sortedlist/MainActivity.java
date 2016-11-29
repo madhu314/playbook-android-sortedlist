48eed5960e0b980f6ae72959c4fecd7ef7ca2b2c
Madhu <madhu@uncommon.is>
2016-11-29 10:24:39 +0530

    Initialise project

---
+++ app/src/main/java/is/uncommon/playbook/sortedlist/MainActivity.java
@@ -0,0 +1,12 @@
+package is.uncommon.playbook.sortedlist;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
