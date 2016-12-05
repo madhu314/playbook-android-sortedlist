d7e230a2642e6976922c8e7a9e58214626635f54
Madhu <madhu@uncommon.is>
2016-12-05 11:47:25 +0530

    Get started with UI

--- app/src/main/java/is/uncommon/playbook/sortedlist/ArticlesActivity.java
+++ app/src/main/java/is/uncommon/playbook/sortedlist/ArticlesActivity.java
@@ -3,10 +3,10 @@ package is.uncommon.playbook.sortedlist;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class ArticlesActivity extends AppCompatActivity {
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.activity_main);
+    setContentView(R.layout.activity_articles);
   }
 }
